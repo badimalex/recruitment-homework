a8d4396e5cc99bfe2182a42e5b91d7cbe5e3e736
Badichan Dmitriy <badimalex@gmail.com>
2016-08-07 20:28:50 +0300

    Added posts info to the main page

--- spec/features/list_posts_spec.rb
+++ spec/features/list_posts_spec.rb
@@ -7,8 +7,16 @@ feature 'List posts' do
     posts
     visit posts_path
 
+    expect(page).to have_content(I18n.t('post.title'))
+    expect(page).to have_content(I18n.t('post.body'))
+    expect(page).to have_content(I18n.t('post.created_at'))
+    expect(page).to have_content(I18n.t('post.user'))
+
     posts.each do |post|
       expect(page).to have_link(post.title)
+      expect(page).to have_content(post.body)
+      expect(page).to have_content(post.user.email)
+      expect(page).to have_content(post.created_at.strftime("%d.%m.%Y"))
     end
   end
 end
